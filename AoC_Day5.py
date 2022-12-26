fa2c803d957ff8e73b0cf2f0fde573acdbecb509
bklolo <brandonlovatt@gmail.com>
2022-12-25 23:42:23 -0600

    Day5b complete

--- AoC_Day5.py
+++ AoC_Day5.py
@@ -6,11 +6,11 @@ x = open('AoC5.txt').read()
 strip = x.split("\n")
 
 # Grab all Boxes
-tallestStack = 8
+tallestStack = 8                    # change
 boxes = strip[0:tallestStack]
 
 # Populate list of boxes
-totalColumns = 9
+totalColumns = 9                    # change
 columnNumber = 1
 columns = []
 for i in range(0, totalColumns):
@@ -23,41 +23,73 @@ for i in range(0, totalColumns):
     columnNumber += 4
 
 # Grab move commands
-commandsBegin = 10
+commandsBegin = 10                  # change
 commands = strip[commandsBegin:]
 print("commands\n", commands)
 
-# Move amount is at commands[5-6]
-    # If moveAmount is a double-digit:
-        # index of columns += 1
-    # Else
-        # index of fromCol = moveAmount+7, toCol = movement+12
-# Execute commands
-moveAmount = 0
-moveFrom = 0
-moveTo = 0
-offset = 1
-for line in commands:
-    if line[6] != ' ':
-        moveAmount = int(line[5:7])
-        moveFrom = int(line[13]) - offset
-        moveTo = int(line[18]) - offset
-    else:
-        moveAmount = int(line[5])
-        moveFrom = int(line[12]) - offset
-        moveTo = int(line[17]) - offset
+#############################################
+###### Part I: Move crates one-by-one #######
+#############################################
 
-    for amt in range(0, moveAmount):
-        columns[moveTo].append(columns[moveFrom].pop())
-answer = []
-for item in columns:
-    answer.append(item.pop())
+def partIMove():
+    moveAmount = 0
+    moveFrom = 0
+    moveTo = 0
+    offset = 1
+    for line in commands:
+        if line[6] != ' ':
+            moveAmount = int(line[5:7])
+            moveFrom = int(line[13]) - offset
+            moveTo = int(line[18]) - offset
+        else:
+            moveAmount = int(line[5])
+            moveFrom = int(line[12]) - offset
+            moveTo = int(line[17]) - offset
 
-print("answer\n", answer)
-'''pseudo code'''
-# get input from file as string
-# deal with box input
-    # add each row to a list
-    # use substrings to collect letters from rows
-    # put column letters in appropriate lists
-    # deal with moves input
+        for amt in range(0, moveAmount):
+            columns[moveTo].append(columns[moveFrom].pop())
+    answer = []
+    for item in columns:
+        answer.append(item.pop())
+
+    print("answer\n", answer)
+
+# Run commands and print answer
+# partIMove()
+
+#############################################
+###### Part II: Move multiple crates ########
+#############################################
+
+def partIIMove():
+    moveAmount = 0
+    moveFrom = 0
+    moveTo = 0
+    offset = 1
+    for line in commands:
+        multiList = []
+        # check for double digit number
+        if line[6] != ' ':
+            moveAmount = int(line[5:7])
+            moveFrom = int(line[13]) - offset
+            moveTo = int(line[18]) - offset
+        # single digit number
+        else:
+            moveAmount = int(line[5])
+            moveFrom = int(line[12]) - offset
+            moveTo = int(line[17]) - offset
+        # move n crates into list
+        for amt in range(0, moveAmount):
+            multiList.append(columns[moveFrom].pop())
+        multiList.reverse()
+        for e in multiList: ###### not finished
+            print("e: ", e)
+            columns[moveTo].append(e)
+    answer = []
+    for item in columns:
+        answer.append(item.pop())
+
+    print("answer\n", answer)
+
+# Run commands and print answer
+partIIMove()
