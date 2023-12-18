4100cf55112769f820e262ca0bd645d175baf2a7
Brandon <bkl@utsi.com>
2023-12-18 16:30:45 -0600

    problem 1 complete

---
+++ 2023/test.py
@@ -0,0 +1,21 @@
+import re
+x = open('2023\\AoC1_Input.txt').read()
+document = re.split("\n", x)
+
+row = []
+for line in document:
+    current_number = ""
+    for letter in line:
+        if letter.isnumeric():
+            current_number += letter
+    if current_number:
+        row.append(current_number)
+print(row)
+sum=0
+for item in row:
+    first = item[0]
+    last = item[-1]
+    num = first+last
+    sum += int(num)
+    print("num= ",num,"sum= ",sum)
+
